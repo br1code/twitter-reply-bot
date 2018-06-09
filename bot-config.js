c90d2f0b1f6bfb0a941424a825cc7825afbf64dd
br1code <brunogiovagnoli@gmail.com>
2018-06-08 23:01:34 -0300

    add config bot to simplicity

---
+++ bot-config.js
@@ -0,0 +1,26 @@
+// Put the keys and tokens of your Twitter account
+const client = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_KEY,
+    access_token_secret: process.env.TWITTER_ACCES_SECRET
+};
+
+// Put your key words to filter the tweets
+// Example: '#cats, #dogs, meow'
+const track = '';
+
+// Put the user name of your Twitter account
+// Example: 'cat_bot'
+const userName = '';
+
+// Put your possible replies
+// Example: ['your dog is amazing!', 'this cat is so cute!', 'meow meow meow']
+const replies = [];
+
+module.exports = {
+    client,
+    track,
+    userName,
+    replies
+};
--- bot.js
+++ bot.js
@@ -1,39 +1,28 @@
 const Twitter = require('twitter');
 const randomReply = require('./random-reply');
+const botConfig = require('./bot-config');
 
 const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_KEY,
-    access_token_secret: process.env.TWITTER_ACCES_SECRET
+    consumer_key: botConfig.client.consumer_key,
+    consumer_secret: botConfig.client.consumer_secret,
+    access_token_key: botConfig.client.access_token_key,
+    access_token_secret: botConfig.client.access_token_secret
 });
 
-// Put your key words to track the tweets
-// Example: '#cats, #dogs, meow'
-const track = '';
-
-// Put the user name of the Twitter account
-// Example: 'cat_bot'
-const userName = '';
-
-// Put your possible replies
-// Example: ['your dog is amazing!', 'this cat is so cute!', 'meow meow meow']
-const replies = [];
-
 // Config the stream to filter the tweets with our key words
-const stream = client.stream('statuses/filter', {track: track});
+const stream = client.stream('statuses/filter', {track: botConfig.track});
 
 // Each time the stream get a tweet, reply it with a random phrase
 stream.on('data', (tweet) => {
 
     // dont allow reply himself
-    if (tweet.user.screen_name === userName) {
+    if (tweet.user.screen_name === botConfig.userName) {
         return;
     }
     
     // Create the new random reply
     var newReply = {
-        status: randomReply(tweet, replies),
+        status: randomReply(tweet, botConfig.replies),
         in_reply_to_status_id: tweet.id_str
     };
 
